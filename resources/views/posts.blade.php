6b52f323af6a30084d06e93310ae7594658d59b4
Antoine LRK <antoinelrk@icloud.com>
2023-12-20 10:25:39 +0100

    Finish EP 12 of Laracast course

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,14 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
+    <h1>My Blog</h1>
     <?php foreach ($posts as $post): ?>
-        <?= $post; ?>
+        <h2>
+            <a href="/posts/<?= $post->slug ?>"><?= $post->title ?></a>
+        </h2>
+        <div>
+            <?= $post->excerpt ?>
+        </div>
     <?php endforeach; ?>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,12 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
+    $posts = Post::all();
+
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
