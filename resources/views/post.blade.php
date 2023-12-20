36912e97a868c45dcf42cf003f9ef41eacba353e
Antoine LRK <antoinelrk@icloud.com>
2023-12-20 11:22:49 +0100

    Finished AP 15 of Laracast course

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $post->title }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body class="flex flex-col gap-y-8 p-3">
+<x-layout>
     <a class="w-max underline text-slate-500/70" href="/">Go Back</a>
     <article class="flex flex-col gap-y-1">
         <h2 class="text-2xl">{{ $post->title }}</h2>
         <p class="rounded bg-slate-300/30 p-4">{{ $post->body }}</p>
     </article>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body class="flex flex-col gap-y-8 p-3">
-    <h1 class="text-3xl">My Blog</h1>
+<x-layout>
     @foreach ($posts as $post)
     <article class="flex flex-col gap-y-1 rounded bg-slate-300/30 p-4">
         <h2 class="text-2xl"> {{ $post->title }} </h2>
@@ -16,8 +6,7 @@
             {{ $post->excerpt }}
         </div>
         <a class="bg-blue-600 hover:bg-orange-600 mt-8 px-2 py-1 rounded m-x-auto w-max text-white" href="/posts/{{ $post->slug }}">Read more</a>
-
+    
     </article>
     @endforeach
-</body>
-</html>
+</x-layout>
