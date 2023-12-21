061acb01745eedac521914c7e758adcd72519ec2
Antoine LRK <antoinelrk@icloud.com>
2023-12-21 10:05:16 +0100

    Added seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'title' => 'My <strong>First</strong> Post',
+            'slug' => Str::slug('My First Post'),
+            'user_id' => rand(1, 5),
+            'category_id' => $personal->id,
+            'excerpt' => 'Lorem ipsum <strong>dolor</strong> sit amet.',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ]);
+
+        Post::create([
+            'title' => 'Laravel is <strong>Amazing</strong>',
+            'slug' => Str::slug('Laravel is Amazing'),
+            'user_id' => rand(1, 5),
+            'category_id' => $work->id,
+            'excerpt' => 'Lorem ipsum <strong>dolor</strong> sit amet.',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ]);
+
+        Post::create([
+            'title' => 'My <strong>Third</strong> Post',
+            'slug' => Str::slug('My Third Post'),
+            'user_id' => rand(1, 5),
+            'category_id' => $personal->id,
+            'excerpt' => 'Lorem ipsum <strong>dolor</strong> sit amet.',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ]);
+
+
+        Post::create([
+            'title' => 'Best Holiday is <u>Spring</u> !',
+            'slug' => Str::slug('Best Holiday is Spring !'),
+            'user_id' => rand(1, 5),
+            'category_id' => $family->id,
+            'excerpt' => 'Lorem ipsum <strong>dolor</strong> sit amet.',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ]);
     }
 }
