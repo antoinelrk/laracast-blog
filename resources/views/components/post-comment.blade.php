6599af12392c60a33e47fd07195fbb76595f853b
Antoine LRK <antoinelrk@icloud.com>
2024-01-02 14:37:54 +0100

    Implement comment design

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,20 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="">
-    </div>
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p clas="text-xs">
-                Posted on 
-                <time>{{ $comment->created_at->diffForHumans() }}</time>
-            </p>
-        </header>
-
-        <p>{!! $comment->body !!}</p>
-    </div>
-</article>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="">
+        </div>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p clas="text-xs">
+                    Posted on 
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+    
+            <p>{!! $comment->body !!}</p>
+        </div>
+    </article>
+</x-panel>
