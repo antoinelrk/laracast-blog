660abfa67f5b71256b56df3d93b96dd6e6fde649
Antoine LRK <antoinelrk@icloud.com>
2024-01-03 16:09:38 +0100

    Create forms components and refacto

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -2,5 +2,5 @@
     {{ $attributes(['class' => "bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"]) }}
     type="submit"
     >
-    TO REMOVE {{ $slot }}
+    {{ $slot }}
 </button>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,78 +6,15 @@
         <x-panel>
             <form action="{{ route('admin.posts.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-    
-                <div class="mb-6">
-                    <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Title
-                    </label>
-                    <input
-                        type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="title"
-                        id="title"
-                        value="{{ old('title') }}"
-                        required
-                    >
-    
-                    @error('title')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
-                <div class="mb-6">
-                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Thumbnail
-                    </label>
+                <x-form.input name="title" />
+                <x-form.input name="thumbnail" type="file" />
+                <x-form.textarea name="excerpt" />
+                <x-form.textarea name="body" />
 
-                    <input
-                        type="file"
-                        name="thumbnail"
-                        id="thumbnail"
-                    >
-                </div>
+                <x-form.field>
+                    <x-form.label name="category" />
 
-                <div class="mb-6">
-                    <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Excerpt
-                    </label>
-    
-                    <textarea
-                        class="border border-gray-400 p-2 w-full"
-                        name="excerpt"
-                        id="excerpt"
-                        value="{{ old('excerpt') }}"
-                        required
-                    ></textarea>
-                
-                    @error('excerpt')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Body
-                    </label>
-    
-                    <textarea
-                        class="border border-gray-400 p-2 w-full"
-                        name="body"
-                        id="body"
-                        value="{{ old('body') }}"
-                        required
-                    ></textarea>
-                
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Category
-                    </label>
-    
                     <select
                         name="category_id"
                         required>
@@ -86,13 +23,11 @@
                             <option value="{{ $category->id }} {{ old('category_id') == $category->id ? 'selected' : '' }}">{{ ucfirst($category->name) }}</option>
                         @endforeach
                     </select>
-                
-                    @error('category_id')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
-                <x-submit-button>Publish</x-submit-button>
+                    <x-form.error name="category" />
+                </x-form.field>
+
+                <x-form.button>Publish</x-form.button>
             </form>
         </x-panel>
     </section>
